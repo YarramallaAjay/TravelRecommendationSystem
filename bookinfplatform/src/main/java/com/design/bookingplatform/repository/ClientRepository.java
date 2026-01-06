5fa74090acfc8f008d665d05b2bcdfb994fd0666
Ajay Yarramalla <yarramallaajay@gmail.com>
2026-01-06 14:49:02 +0530

    added booking platform and recommender services with DTO

---
+++ bookinfplatform/src/main/java/com/design/bookingplatform/repository/ClientRepository.java
@@ -0,0 +1,4 @@
+package com.design.bookingplatform.repository;
+
+public class ClientRepository {
+}
---
+++ bookinfplatform/src/main/java/com/design/bookingplatform/service/TrainService.java
@@ -0,0 +1,4 @@
+package com.design.bookingplatform.service;
+
+public class NotificationService {
+}
