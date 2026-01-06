3f01dc8eb44ba8c33aefa7a811704dd31e25ea3b
Ajay Yarramalla <yarramallaajay@gmail.com>
2026-01-06 15:32:14 +0530

    added service based file

---
+++ bookingplatform/src/main/java/com/design/bookingplatform/BookinfplatformApplication.java
@@ -0,0 +1,13 @@
+package com.design.bookingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinfplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookinfplatformApplication.class, args);
+    }
+
+}
